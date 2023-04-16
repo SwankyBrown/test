36e3ab32cd4279290f3cc5eb33672bcd6ece8ae0
Sheldon Christiansen <bigbrownguy724@gmail.com>
2023-04-16 17:03:53 -0600

    assesment update

--- arrays.js
+++ arrays.js
@@ -40,8 +40,15 @@ const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 // CODE HERE
 let answers = []
 
-for(let i = 0; i < 100; ++i){
-
+for(let i = 0; i < bigOrSmallArray.length; ++i){
+if ((bigOrSmallArray[i]) <= 100) {
+  answers.push(bigOrSmallArray[i])
 }
+  console.log(bigOrSmallArray[i])
+  }
+
+
+
+
 
 console.log (answers)
